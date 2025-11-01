26f9179b403e195b6e77d539df46708c059daf17
Aaditya Bajgain <adityabajgain@gmail.com>
2025-11-01 10:12:28 -0700

    setting gemini api

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -1,14 +1,16 @@
 import { GoogleGenAI } from "@google/genai";
 import { NextResponse } from "next/server";
 
-const ai = new GoogleGenAI({});
+const ai = new GoogleGenAI({
+    apiKey:process.env.GOOGLE_GEMINI_API_KEY,
+});
 
-async function GET() {
+export async function GET() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: "write a small rap song about hackathon",
   });
 
-  NextResponse.json(response.text);
+  return NextResponse.json(response.text);
 }
 
